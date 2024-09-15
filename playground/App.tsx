b768fa84306dfb606ecf6fd289f624c3b9e81476
arildwork <arildwork@gmail.com>
2024-09-15 21:27:40 +0200

    Fixed general issues

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FACButton from "../src/components/Button/Button";
+import FACInput from "../src/components/Input/Input";
 
 function App() {
   return (
     <div>
       <h1>Field App Components Playground</h1>
       <FACButton label="Click Me" onClick={() => alert("Button Clicked!")} />
+      <FACInput label='test input' type="password" />
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,7 @@ export default defineConfig(({ mode }) => {
           },
         },
       },
+      cssCodeSplit: false,
     },
     server: {
       port: 5555,
